16e298f6b6a5f00b96b31a777e2977b54ca4a81b
abhijeet-t09 <tripathi.abhijeet0996@gmail.com>
2022-08-30 12:47:25 +0530

    Simple Linked List

---
+++ src/com/bridgelabz/LinkedList.java
@@ -0,0 +1,28 @@
+package com.bridgelabz;
+
+public class LinkedList<T> {
+    Node<T> head;
+    Node<T> tail;
+
+    void push(T data) {
+        Node<T> node = new Node<>(data);
+
+        if (head == null) {
+            head = node;
+            tail = node;
+        }
+        else {
+            node.next = head;
+            head = node;
+        }
+    }
+
+    void print() {
+        Node<T> temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+}
--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -2,6 +2,14 @@ package com.bridgelabz;
 
 public class LinkedListMain {
     public static void main(String[] args) {
-        System.out.println("|-- Linked List Program --|");
+        System.out.println("|-- Linked List Program --|\n");
+
+        LinkedList<Integer> linkedList = new LinkedList<>();
+
+        linkedList.push(70);
+        linkedList.push(30);
+        linkedList.push(56);
+
+        linkedList.print();
     }
 }
